60da4a45508e23fe57b2c766d3ea1a20f8be3d7e
matsuura0831 <matsuura0831@gmail.com>
2020-04-04 20:18:34 +0900

    add tutorial files.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    title: 'besolab',
+    descrpition: 'A place to store technical knowledge for myself.',
+    locales: {
+        '/': { lang: 'ja' },
+    },
+    themeConfig: {
+        sidebar: 'auto',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'About', link: '/about/' },
+            {
+                text: 'SNS',
+                items: [
+                    { text: 'Twitter', link: 'https://twitter.com/matsuura0831/' },
+                    { text: 'GitHub', link: 'https://github.com/matsuura0831/' }
+                ]
+            }
+        ]
+    },
+}
