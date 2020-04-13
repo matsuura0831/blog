f2bb17f6d533f37df3bb4bcd75e0e1385bebac13
matsuura0831 <matsuura0831@gmail.com>
2020-04-14 02:50:15 +0900

    update about page.

---
+++ docs/.vuepress/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-gray-100 py-12 flex justify-center">
+        <div class="bg-white mx-8 flex lg:max-w-5xl lg:shadow-lg lg:rounded-lg">
+            <div class="lg:w-1/2">
+                <div class="h-60 bg-center bg-cover lg:rounded-lg lg:h-full"
+                  :style="'background-image:url(\'' + image + '\')'"></div>
+            </div>
+            <div class="py-12 px-6 max-w-xl lg:max-w-5xl lg:w-1/2">
+                <h2 class="text-3xl text-gray-800 font-bold">Build Your New <span class="text-indigo-600">Idea</span></h2>
+                <p class="mt-4 text-gray-600">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem modi reprehenderit vitae exercitationem aliquid dolores ullam temporibus enim expedita aperiam mollitia iure consectetur dicta tenetur, porro consequuntur saepe accusantium consequatur.</p>
+                <div class="mt-8">
+                    <a href="#" class="bg-gray-900 text-gray-100 px-5 py-3 font-semibold rounded">Start Now</a>
+                </div>
+            </div>
+        </div>
+    </div> 
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    text: String
+  },
+};
+</script>
--- docs/about/index.md
+++ docs/about/index.md
@@ -1,12 +1,25 @@
+---
+sidebar: false
+---
+
 # About
 
-## Posts
+## ME
 
-- [x] GoogleSearch
-- [x] GoogleAnalysis
-- [x] markdown setting
-- [ ] category pages
-- [ ] tag pages
+<ul class="flex flex-row mt-4">
+    <li><img class="h-20" src="https://avatars.githubusercontent.com/matsuura0831"></il>
+    <li class="text-4xl font-bold my-auto ml-4"><span>matsuura0831</span></li>
+</ul>
 
 
-<Posts />
+## TODO
+
+- [x] install GoogleSearch plugin
+- [x] install GoogleAnalysis plugin
+- [x] setup markdown
+- [x] use tailwindcss
+- [x] use fontawaresome
+- [x] add category page
+- [x] add tag page
+- [x] create about page
+- [ ] write articles on the above content
