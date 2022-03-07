53179917dda897b5b37d7a1136280d063b0a9c07
Nobuhiko Matsuura <matsuura0831@gmail.com>
2022-03-07 15:18:37 +0000

    disable sns.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -61,13 +61,6 @@ module.exports = {
         nav: [
             { text: 'Home', link: '/' },
             { text: 'About', link: '/about/' },
-            {
-                text: 'SNS',
-                items: [
-                    { text: 'Twitter', link: 'https://twitter.com/matsuura0831/' },
-                    { text: 'GitHub', link: 'https://github.com/matsuura0831/' }
-                ]
-            }
         ]
     },
     postcss: {
